4b8a7961404460ced9d055c53efa0b079e2c8c25
ZakirBangash <engrzakirbangash@gmail.com>
2020-06-21 17:16:16 +0500

    updated UI

--- src/IncomeExpense.js
+++ src/IncomeExpense.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const IncomeExpense = () => {
     return (
         <div className="Expense">
-            <h3>Income <br/> $400</h3>
-             <h3>Expense <br/>$200</h3>
+            <h3 >Income <br/> <span className="incomeC">$400</span></h3>
+             <h3>Expense <br/> <span className="expenseC">$200</span></h3>
         </div>
     )
 }
